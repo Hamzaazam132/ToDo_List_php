bdc1d7cd1580c568378ca77064e08db15f846cf2
Hamzaazam132 <Hamzaazam132@gmail.com>
2023-10-25 02:09:50 +0500

    Some Changes In The Files Name

--- db.php
+++ db.php
@@ -8,5 +8,4 @@ $dbcon = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
 if ($dbcon->connect_error) {
   die("connect error: " . $dbcon->connect_error);
 }
-
 ?>
--- header.php
+++ header.php
@@ -7,13 +7,12 @@
     body{
     padding:0;
     margin:0;
-    background:url('background.jpg');
+    background:url('background-image.jpg');
     background-size:cover;
  
     background-repeat:no-repeat;
     }
     </style>
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   
